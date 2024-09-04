88133130fb17ab0404cc3d0a0ab84a75ddedec0f
Jay <jay.singh@philips.com>
2024-09-04 22:39:24 +0530

    fix review comments

--- ColorPair_PairNumberMapTests.h
+++ ColorPair_PairNumberMapTests.h
@@ -1,15 +1,32 @@
 #pragma once
 
 #include "ColorPair.h"
+#include "ColorPair_PairNumberMapTests.h"
+#include "ColorPair_PairNumberMap.h"
+#include <iostream>
+#include <assert.h>
 
 namespace TelCoColorCoder
 {
     class ColorPair_PairNumberMapTests
     {
-        public:
+    public:
         ColorPair_PairNumberMapTests() = default;
         ~ColorPair_PairNumberMapTests() = default;
-        void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor);
-        void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber);
+
+        static void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor)
+        {
+            auto colorPair = TelCoColorCoder::ColorPair_PairNumberMap::GetColorFromPairNumber(pairNumber);
+            std::cout << "Got pair " << colorPair.ToString() << std::endl;
+            assert(colorPair.getMajor() == expectedMajor);
+            assert(colorPair.getMinor() == expectedMinor);
+        }
+
+        static void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber)
+        {
+            int pairNumber = TelCoColorCoder::ColorPair_PairNumberMap::GetPairNumberFromColor(major, minor);
+            std::cout << "Got pair number " << pairNumber << std::endl;
+            assert(pairNumber == expectedPairNumber);
+        }
     };
 }
--- ColorTypes.h
+++ ColorTypes.h
@@ -5,6 +5,18 @@
 
 namespace TelCoColorCoder
 {
+    // color names
+    static constexpr auto WHITE_COLOR_NAME = "White";
+    static constexpr auto RED_COLOR_NAME = "Red";
+    static constexpr auto BLACK_COLOR_NAME = "Black";
+    static constexpr auto YELLOW_COLOR_NAME = "Yellow";
+    static constexpr auto VIOLET_COLOR_NAME = "Violet";
+    static constexpr auto BLUE_COLOR_NAME = "Blue";
+    static constexpr auto ORANGE_COLOR_NAME = "Orange";
+    static constexpr auto GREEN_COLOR_NAME = "Green";
+    static constexpr auto BROWN_COLOR_NAME = "Brown";
+    static constexpr auto SLATE_COLOR_NAME = "Slate";
+
     enum MajorColor
     {
         WHITE,
@@ -22,31 +34,32 @@ namespace TelCoColorCoder
         SLATE
     };
 
+    
     const char * const MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"};
+        WHITE_COLOR_NAME, RED_COLOR_NAME, BLACK_COLOR_NAME, YELLOW_COLOR_NAME, VIOLET_COLOR_NAME};
 
     const unsigned numberOfMajorColors =
         sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
     const char * const MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"};
+        BLUE_COLOR_NAME, ORANGE_COLOR_NAME, GREEN_COLOR_NAME, BROWN_COLOR_NAME, SLATE_COLOR_NAME};
 
     const unsigned numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
     const std::map<MajorColor, const char*> MajorColorMap = {
-        {WHITE, "White"},
-        {RED, "Red"},
-        {BLACK, "Black"},
-        {YELLOW, "Yellow"},
-        {VIOLET, "Violet"}
+        {WHITE, WHITE_COLOR_NAME},
+        {RED, RED_COLOR_NAME},
+        {BLACK, BLACK_COLOR_NAME},
+        {YELLOW, YELLOW_COLOR_NAME},
+        {VIOLET, VIOLET_COLOR_NAME}
     };
 
     const std::map<MinorColor, const char*> MinorColorMap = {
-        {BLUE, "Blue"},
-        {ORANGE, "Orange"},
-        {GREEN, "Green"},
-        {BROWN, "Brown"},
-        {SLATE, "Slate"}
+        {BLUE, BLUE_COLOR_NAME},
+        {ORANGE, ORANGE_COLOR_NAME},
+        {GREEN, GREEN_COLOR_NAME},
+        {BROWN, BROWN_COLOR_NAME},
+        {SLATE, SLATE_COLOR_NAME}
     };
 }
