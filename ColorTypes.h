58e31db0aa0a0ec273eb78594387b519a21083a5
Jay <jay.singh@philips.com>
2024-09-04 22:45:20 +0530

    fix 50 loc contraint issue

--- ColorTypes.h
+++ ColorTypes.h
@@ -1,22 +1,11 @@
 #pragma once
 
+#include "ColorNames.h"
 #include <string>
 #include <map>
 
 namespace TelCoColorCoder
 {
-    // color names
-    static constexpr auto WHITE_COLOR_NAME = "White";
-    static constexpr auto RED_COLOR_NAME = "Red";
-    static constexpr auto BLACK_COLOR_NAME = "Black";
-    static constexpr auto YELLOW_COLOR_NAME = "Yellow";
-    static constexpr auto VIOLET_COLOR_NAME = "Violet";
-    static constexpr auto BLUE_COLOR_NAME = "Blue";
-    static constexpr auto ORANGE_COLOR_NAME = "Orange";
-    static constexpr auto GREEN_COLOR_NAME = "Green";
-    static constexpr auto BROWN_COLOR_NAME = "Brown";
-    static constexpr auto SLATE_COLOR_NAME = "Slate";
-
     enum MajorColor
     {
         WHITE,
